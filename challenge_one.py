b680791fd8a4bb71892eb61fcb5575918490fb0e
Nestor Masinde <nestormasinde@gmail.com>
2024-03-02 17:59:58 +0300

    WIP: challenge one functionality

--- challenge_one.py
+++ challenge_one.py
@@ -6,3 +6,32 @@
 
 # that, given an array A of N integers, returns the minimum number of moves needed to end up with exactly 10 bricks in every box. If this is not possible, the function should return −1.
 
+def solution(A):
+    total_bricks = sum(A)
+    target_sum = 10 * len(A)
+    difference = abs(target_sum - total_bricks)
+
+    if difference % 2 != 0:
+        return -1
+    
+    running_sum = 0
+    excess_bricks = 0
+
+    for i in range(len(A)):
+        current_bricks = A[i]
+        needed_bricks = 10 - current_bricks
+        excess_bricks += needed_bricks
+
+        if excess_bricks > 0:
+            moves = min(excess_bricks, current_bricks)
+            running_sum += moves
+            current_bricks -= moves
+            excess_bricks -= moves
+
+        running_sum += abs(current_bricks)
+            
+    return running_sum
+ 
+
+bricks_in_boxes = [7, 15, 10, 8]
+print(solution(bricks_in_boxes))
